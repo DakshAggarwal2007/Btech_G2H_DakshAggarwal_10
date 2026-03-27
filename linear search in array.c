5fffdf8ce0d67ed08de383c12852bdac1dc526d6
DakshAggarwal2007 <daksh25102007@gmail.com>
2026-03-27 10:46:47 +0530

    Create linear search in array.c

---
+++ linear search in array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,j,item;
+    printf("Enter the no. of elements in the array:");
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the element to search:");
+    scanf("%d",&item);
+    for(j=0;j<n;j++){
+        if(a[j]==item){
+            printf("Item is found at index %d\n",j);
+        }
+    }
+    return 0;
+}
