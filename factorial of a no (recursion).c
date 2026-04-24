1ccca628c5bb2dda670fd71153e3c4d53d7db312
DakshAggarwal2007 <daksh25102007@gmail.com>
2026-04-24 09:19:35 +0530

    Create factorial of a no (recursion).c

---
+++ factorial of a no (recursion).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int fact(int a){
+    if (a==0){
+        return 1;
+    }
+    else{
+        return (a*fact(a-1));
+    }
+}
+int main() {
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    printf("Factorial of the number is %d:",fact(n));
+    return 0;
+}
